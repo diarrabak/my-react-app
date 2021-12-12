2459c17a500941eeee5ee8159b69ed583fbd4bcc
diarrabak <71572448+diarrabak@users.noreply.github.com>
2021-12-12 19:34:31 +0000

    Adding components Description and Header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import picture from "./images/sport-canada.png";
 import "./App.css";
 
+import Description from "./Description";
+import Header from "./Header";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1> Canadian sports</h1>
         <img src={picture} className="App-logo" alt="sports in Canada" />
-        <p>
-          Canada has a wide variety of games. It ranges from summer to winter
-          sports. The most common sports are ice hockey, lacrosse, soccer,
-          basketball, and ice hockey.
-        </p>
+        <Description />
+        <div className="buttons">
+          <button onClick={(e) => alert(e.target.innerHTML+" clicked!")}>Ice Hockey</button>
+          <button onClick={(e) => alert(e.target.innerHTML+" clicked!")}>Basketball</button>
+          <button onClick={(e) => alert(e.target.innerHTML+" clicked!")}>Soccer</button>
+        </div>
       </header>
     </div>
   );
