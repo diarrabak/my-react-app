4d077934abbb7987d6dd5b8a8ba3e43eac7546f4
diarrabak <71572448+diarrabak@users.noreply.github.com>
2021-12-12 19:55:52 +0000

    Using the state

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import "./App.css";
 
 import Description from "./Description";
 import Header from "./Header";
+import { useState } from "react";
 
 function App() {
+  let [title, setTitle] = useState("Canadian sports");
   return (
     <div className="App">
       <header className="App-header">
-        <h1> Canadian sports</h1>
+        <Header title={title} />
         <img src={picture} className="App-logo" alt="sports in Canada" />
-        {/* Replace the paragraph by the description component */}
         <Description />
         <div className="buttons">
-          <button onClick={(e) => alert(e.target.innerHTML+" clicked!")}>Ice Hockey</button>
-          <button onClick={(e) => alert(e.target.innerHTML+" clicked!")}>Basketball</button>
-          <button onClick={(e) => alert(e.target.innerHTML+" clicked!")}>Soccer</button>
+          <button onClick={(e) => setTitle(e.target.innerHTML)}>
+            Ice Hockey
+          </button>
+          <button onClick={(e) => setTitle(e.target.innerHTML)}>
+            Basketball
+          </button>
+          <button onClick={(e) => setTitle(e.target.innerHTML)}>Soccer</button>
         </div>
       </header>
     </div>
