9080747290b9396165cf58f97d60b4ed156f3e67
diarrabak <71572448+diarrabak@users.noreply.github.com>
2021-12-12 20:19:37 +0000

    Button text display on click event

--- src/App.js
+++ src/App.js
@@ -6,21 +6,28 @@ import Header from "./Header";
 import { useState } from "react";
 
 function App() {
-  let [title, setTitle] = useState("Canadian sports");
+  let [btnText, setBtnText] = useState("");
   return (
     <div className="App">
       <header className="App-header">
-        <Header title={title} />
+        <Header title="Canadian sports" />
         <img src={picture} className="App-logo" alt="sports in Canada" />
         <Description />
         <div className="buttons">
-          <button onClick={(e) => setTitle(e.target.innerHTML)}>
+          <button onClick={(e) => setBtnText(e.target.innerHTML)}>
             Ice Hockey
           </button>
-          <button onClick={(e) => setTitle(e.target.innerHTML)}>
+          <button onClick={(e) => setBtnText(e.target.innerHTML)}>
             Basketball
           </button>
-          <button onClick={(e) => setTitle(e.target.innerHTML)}>Soccer</button>
+          <button onClick={(e) => setBtnText(e.target.innerHTML)}>
+            Soccer
+          </button>
+        </div>
+
+        <div className="btn-text">
+          <h2>Favourite Sports</h2>
+          <h3>{btnText}</h3>
         </div>
       </header>
     </div>
